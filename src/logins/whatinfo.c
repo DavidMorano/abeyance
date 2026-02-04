91dd1dc376c725f96425123e2ea1e123d136ef09
David A.D. Morano <david.a.morano@gmail.com>
2026-02-04 00:47:28 -0500

    update

--- src/logins/whatinfo.c
+++ src/logins/whatinfo.c
@@ -1,12 +1,15 @@
-/* whatinfo */
+/* whatinfo SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 /* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
 
 
+#include	<envstandards.h>	/* ordered first to configure */
 #include	"config.h"
 
 
-static const char	whatinfo[] = WHATINFO VERSION ;
+static cchar	whatinfo[] = WHATINFO VERSION ;
 
 
