efc7e91c48e1556dc88670a56882658e44178429
David A.D. Morano <david.a.morano@gmail.com>
2026-02-04 00:46:36 -0500

    restore

---
+++ src/sun_telnetd/whatinfo.cc
@@ -0,0 +1,15 @@
+/* whatinfo SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	"config.h"
+
+
+static cchar	whatinfo[] = WHATINFO VERSION ;
+
+
