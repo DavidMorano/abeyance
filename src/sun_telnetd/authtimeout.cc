7261d14dd7d74ffa5f030f581279bfcb3a832638
David A.D. Morano <david.a.morano@gmail.com>
2026-02-04 00:46:18 -0500

    restore

---
+++ src/sun_telnetd/authtimeout.cc
@@ -0,0 +1,124 @@
+/* authtimeout SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* authorization time-out */
+/* version %I% last-modified %G% */
+
+#define	CF_DEBUGS	0		/* compile-time debug print-outs */
+
+/* revision history:
+
+	= 1998-05-01, David Morano
+	This was created along with the DATE object.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	authtimeout
+
+	Description:
+	This small code piece provides for authoriation time-outs.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<sys/param.h>
+#include	<sys/stat.h>
+#include	<sys/mman.h>
+#include	<limits.h>
+#include	<unistd.h>
+#include	<string.h>
+#include	<stdarg.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<uclibmem.h>
+#include	<bfile.h>
+#include	<estrings.h>
+#include	<vecobj.h>
+#include	<tmtime.hh>
+#include	<filer.h>
+#include	<storebuf.h>
+#include	<ptma.h>
+#include	<ptm.h>
+#include	<endian.h>
+#include	<sfx.h>
+#include	<timesr.h>
+#include	<localmisc.h>
+
+
+/* local defines */
+
+#define	BABYCALCS_POSTFIXLEN	7
+#define	BABYCALCS_ENTRY		struct babycalcs_e
+#define	BABYCALCS_PERMS		0666
+
+#ifndef	LINEBUFLEN
+#ifdef	LINE_MAX
+#define	LINEBUFLEN	MAX(LINE_MAX,2048)
+#else
+#define	LINEBUFLEN	2048
+#endif
+#endif
+
+#ifndef	SHMNAMELEN
+#define	SHMNAMELEN	14		/* shared-memory name length */
+#endif
+
+#ifndef	SHMPREFIXLEN
+#define	SHMPREFIXLEN	8
+#endif
+
+#ifndef	SHMPOSTFIXLEN
+#define	SHMPOSTFIXLEN	4
+#endif
+
+#define	HDRBUFLEN	(sizeof(BABIESFU) + MAXNAMELEN)
+
+#ifndef	TO_WAITSHM
+#define	TO_WAITSHM	20		/* seconds */
+#endif
+
+#define	TO_LASTCHECK	5		/* seconds */
+#define	TO_DBWAIT	1		/* seconds */
+#define	TO_DBPOLL	300		/* milliseconds */
+
+#define	SHIFTINT	(6 * 60)	/* possible time-shift */
+
+
+/* external subroutines */
+
+#if	CF_DEBUGS
+extern int	debugprintf(cchar *,...) noex ;
+extern int	strlinelen(cchar *,int,int) noex ;
+#endif
+
+
+/* exported variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int authtimeout() noex {
+    	int		rs = SR_OK ;
+    	return rs ;
+}
+
+
