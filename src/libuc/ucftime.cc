730b484781afc0bc978a194c42f8d82cf81d764a
David A.D. Morano <david.a.morano@gmail.com>
2026-02-14 00:51:57 -0500

    update

--- src/libuc/ucftime.cc
+++ src/libuc/ucftime.cc
@@ -101,11 +101,11 @@ int uc_ftime(TIMEB *tbp) noex {
 		    custime 	t = tbp->time ;
 	            tbp->time = tv.tv_sec ;
 	            tbp->millitm = (tv.tv_usec / 1000) ;
-	            if (tmtime tmt ; (rs = tmtime_localtime(&tmt,t)) >= 0) {
+	            if (tmtime tmt ; (rs = tmtime_timelocal(&tmt,t)) >= 0) {
 	                tbp->timezone = (tmt.gmtoff / 60) ;
 	                tbp->dstflag = tmt.isdst ;
 	                rs = (tmt.isdst > 0) ? 1 : 0 ;
-	            } /* end if (tmtime_localtime) */
+	            } /* end if (tmtime_timelocal) */
 	        } /* end if (uc_gettimeofday) */
 	    } /* end if_constexpr (f_darwin) */
 	} /* end if (non-null) */
