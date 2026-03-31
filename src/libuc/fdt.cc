efacd6be9067ba1b044eacd83c536d8499bb6354
David A.D. Morano <david.a.morano@gmail.com>
2026-03-31 01:21:16 -0400

    update

--- src/libuc/fdt.cc
+++ src/libuc/fdt.cc
@@ -19,7 +19,8 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<vecobj.h>
 #include	<localmisc.h>
 
