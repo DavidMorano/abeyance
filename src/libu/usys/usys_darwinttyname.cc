317c9fb24ac1d94f3cf28c9a4068ee8f180b8c8f
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:10:40 -0400

    update

--- src/libu/usys/usys_darwinttyname.cc
+++ src/libu/usys/usys_darwinttyname.cc
@@ -164,7 +164,7 @@ int usysdarwinttyr::init() noex {
                 auto lamb = [this] () -> int {
                     int         rsl = SR_OK ;
                     if (!finit) {
-                        rsl = SR_LOCKLOST ;              /* <- failure */
+                        rsl = SR_LOCKFAIL ;              /* <- failure */
                     } else if (finitdone) {
                         rsl = 1 ;                        /* <- OK ready */
                     }                       
