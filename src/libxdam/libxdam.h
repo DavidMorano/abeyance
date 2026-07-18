6944e78776c8a1cefde2857b63ed6abbe932ca1b
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:30:31 -0400

    restore

---
+++ src/libxdam/libxdam.h
@@ -0,0 +1,32 @@
+/* lib_dam HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* general purpose utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#ifndef	LIBXDAM_INCLUDE
+#define	LIBXDAM_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<progbal.h>
+
+
+#endif /* LIBXDAM_INCLUDE */
+
+
