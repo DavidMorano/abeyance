e6f8a5d9ed3e54d4a909e496160e58184a6db82f
David A.D. Morano <david.a.morano@gmail.com>
2026-09-17 00:25:17 -0400

    restore

---
+++ src/libxmisc/li.h
@@ -0,0 +1,46 @@
+/* li HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* long (64 bit) interger arithmetic object module */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LI_INCLUDE
+#define	LI_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+
+/* object defines */
+#define	LI	struct longint_head
+
+
+struct longint_head {
+	long		u, l ;
+} ;
+
+typedef LONGINT		longint ;
+
+EXTERNC_begin
+
+extern int li_load	(LI *,int,int) noex ;
+extern int li_store	(LI *,int *,int *) noex ;
+extern int li_zero	(LI *) noex ;
+extern int li_add2	(LI *,LI *) noex ;
+extern int li_add3	(LI *,LI *,LI *) noex ;
+
+EXTERNC_end
+
+
+#endif /* LI_INCLUDE */
+
+
